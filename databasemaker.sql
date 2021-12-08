807df977bb47bd9583b64f40f9e0dd437cffa855
FrancisIris <93911682+FrancisIris@users.noreply.github.com>
2021-12-08 11:48:29 +0000

    Populating Database tables

--- databasemaker.sql
+++ databasemaker.sql
@@ -1,29 +1,71 @@
-DROP TABLE if EXISTS Borrowed;
-DROP TABLE if EXISTS Accounts;
-DROP TABLE if EXISTS Books;
-DROP TABLE if EXISTS BookInfo;
-CREATE TABLE Accounts(
+DROP TABLE if EXISTS Borrowed
+DROP TABLE if EXISTS Accounts
+DROP TABLE if EXISTS Books
+DROP TABLE if EXISTS BookInfo
 
+CREATE TABLE Accounts(
 ID int NOT NULL PRIMARY KEY,
 AccountName varchar(255),
 AccountPassword varchar(255),
-
-);
+)
 
 CREATE TABLE BookInfo(
 ISBN int NOT NULL PRIMARY KEY,
 BookName varchar(255),
 Author varchar(255),
 BarCode int
-);
+)
 CREATE TABLE Books(
 ID int NOT NULL PRIMARY KEY,
 ISBN int NOT NULL FOREIGN KEY REFERENCES BookInfo(ISBN)
-);
+)
 CREATE TABLE Borrowed(
-userID int NOT NULL FOREIGN KEY REFERENCES Accounts(ID),
-bookID int NOT NULL FOREIGN KEY REFERENCES Books(ID),
-duedate datetime,
+UserID int NOT NULL FOREIGN KEY REFERENCES Accounts(ID),
+BookID int NOT NULL FOREIGN KEY REFERENCES Books(ID),
+Duedate datetime,
 CONSTRAINT PK_Borrowed PRIMARY KEY (userID,bookID)
 );
 
+BULK INSERT Accounts
+FROM 'C:\Users\Francis.Jordan\OneDrive\Desktop\Learning\Bootcamp\Bookish\AccountsData.csv'
+WITH
+(
+	FIRSTROW = 2, -- as 1st one is header
+    FIELDTERMINATOR = ',',  --CSV field delimiter
+    ROWTERMINATOR = '\n',   --Use to shift the control to next row
+    TABLOCK
+)
+
+BULK INSERT BookInfo
+FROM 'C:\Users\Francis.Jordan\OneDrive\Desktop\Learning\Bootcamp\Bookish\BookInfoData.csv'
+WITH
+(
+	FIRSTROW = 2, -- as 1st one is header
+    FIELDTERMINATOR = ',',  --CSV field delimiter
+    ROWTERMINATOR = '\n',   --Use to shift the control to next row
+    TABLOCK
+)
+
+BULK INSERT Books
+FROM 'C:\Users\Francis.Jordan\OneDrive\Desktop\Learning\Bootcamp\Bookish\BooksData.csv'
+WITH
+(
+	FIRSTROW = 2, -- as 1st one is header
+    FIELDTERMINATOR = ',',  --CSV field delimiter
+    ROWTERMINATOR = '\n',   --Use to shift the control to next row
+    TABLOCK
+)
+
+BULK INSERT Borrowed
+FROM 'C:\Users\Francis.Jordan\OneDrive\Desktop\Learning\Bootcamp\Bookish\BorrowedData.csv'
+WITH
+(
+	FIRSTROW = 2, -- as 1st one is header
+    FIELDTERMINATOR = ',',  --CSV field delimiter
+    ROWTERMINATOR = '\n',   --Use to shift the control to next row
+    TABLOCK
+)
+select * from Accounts;
+select * from BookInfo;
+select * from Books;
+select * from Borrowed;
